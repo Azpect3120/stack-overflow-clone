4070fb137576fa987b465a3410a7fde769dd435d
Hayden Hargreaves <hhargr836@west-mec.org>
2023-10-13 14:27:06 -0700

    BUilding routes for users

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const bodyParser = require("body-parser")
+const axios = require("axios")
+require("dotenv").config()
+
+
+router.post("/create", async(req, res) => {
+    const { username, password } = req.body;
+    const appId = process.env.APP_ID;
+
+    const response = await axios.post(process.env.AUTH_URL + "/createUser", {
+        applicationID: appId,
+        username,
+        password
+    })
+
+    console.log(response)
+
+})
+
+router.post("/verify", (req, res) => {
+
+
+})
 
 
 
-
-
-
-
-module.exports = router;
+module.exports = router
