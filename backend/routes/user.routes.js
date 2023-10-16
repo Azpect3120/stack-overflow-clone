e574e89f057faa8c60daa8c8c21336b7972aeff3
Hayden Hargreaves <hhargr836@west-mec.org>
2023-10-16 12:49:08 -0700

    User auth

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,27 +2,39 @@ const express = require('express')
 const router = express.Router()
 const bodyParser = require("body-parser")
 const axios = require("axios")
-require("dotenv").config()
 
 
+/*
+    Send username and password through form  
+*/
 router.post("/create", async(req, res) => {
     const { username, password } = req.body;
-    const appId = process.env.APP_ID;
+    const appId = "501dfdb9-3711-4e49-a180-1ff480b22a43";
 
-    const response = await axios.post(process.env.AUTH_URL + "/createUser", {
+    const response = await axios.post("https://authentication-server-lzo6.onrender.com/users/create", {
         applicationID: appId,
         username,
         password
-    })
+    });
 
-    console.log(response)
+    res.json(response);
+});
 
-})
+/*
+    Send username and password through form
+*/
+router.post("/verify", async (req, res) => {
+    const { username, password } = req.body;
+    const appId = "501dfdb9-3711-4e49-a180-1ff480b22a43";
 
-router.post("/verify", (req, res) => {
+    const response = await axios.post("https://authentication-server-lzo6.onrender.com/users/verify", {
+        applicationID: appId,
+        username,
+        password
+    });
 
-
-})
+    res.json(response);
+});
 
 
 
