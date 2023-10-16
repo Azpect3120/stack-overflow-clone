be61070b7ae11b82cf170320e9d677f8f25cf69e
Nathan Hadden <nhadde445@west-mec.org>
2023-10-16 14:28:48 -0700

    Added form template with errors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Nav";
+import Footer from "./components/Footer";
 function App() {
 
   return (
     <>
       <Navbar /> 
+      <Footer />
     </>
   )
 }
 
-export default App
+export default App;
