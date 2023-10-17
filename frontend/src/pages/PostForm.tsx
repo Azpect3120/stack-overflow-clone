810fcd4c78ea30f513ef7068815ba0ef545d2ecf
Hayden Hargreaves <hhargr836@west-mec.org>
2023-10-17 14:12:26 -0700

    DONT DIE

--- frontend/src/pages/PostForm.tsx
+++ frontend/src/pages/PostForm.tsx
@@ -7,7 +7,23 @@ function PostForm (): JSX.Element {
         <div className="w-full h-fit bg-light-background">
             <Navbar />
         
-            <div className="w-3/5 h-screen mx-auto bg-red-500">
+            <div className="w-2/3 h-screen mx-auto">
+                <h1 className="font-bold text-3xl px-20 py-10">
+                    Create a Post
+                </h1>
+
+                <div className="h-fit w-full rounded-md p-6 bg-white border border-light-border">
+                    <h1 className="text-lg py-2"> Title </h1>
+                    <p className="text-xs"> Enter your posts title </p>
+                    <input className="text-sm w-full my-2 px-2 py-1 border border-light-border rounded-md outline-none focus:ring-1 focus:ring-green-500" type="text" name="title" placeholder="eg. 10 Reasons Why Go is Better Than Rust." required />
+                </div> 
+
+                <div className="h-fit w-full my-5 rounded-md p-6 bg-white border border-light-border">
+                    <h1 className="text-lg py-2"> Post Details </h1>
+                    <p className="text-xs"> Enter your posts details here </p>
+                    <textarea className="text-sm w-full my-2 px-2 py-1 border border-light-border rounded-md outline-none focus:ring-1 focus:ring-green-500" name="description" placeholder="eg. 10 Reasons Why Go is Better Than Rust." required />
+                </div>
+
 
 
             </div>
