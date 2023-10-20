303eb04af20f9feb2efdc87909f3576dd69e9931
Nathan Hadden <nhadde445@west-mec.org>
2023-10-20 13:27:11 -0700

    Added CreateComment component

---
+++ frontend/src/components/CreateComment.tsx
@@ -0,0 +1,62 @@
+import { SyntheticEvent, useState } from "react";
+
+interface CommentObject {
+    author: String;
+    content: String;
+    date: Date;
+    postId: String;
+}
+
+function createComment (): JSX.Element {
+    const ls = localStorage.getItem("user");
+    const author: string = ls ? JSON.parse(ls).username : "";
+
+    let [comment, setComment] = useState<CommentObject>({author, content: "", date: new Date(), postId: ""});
+
+    const handleSubmit = async (event: SyntheticEvent): Promise<void> => {
+        event.preventDefault();
+
+        setComment({ ...comment, date: new Date() });
+
+        try {
+            await fetch("!!!fetchURL", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(comment)
+            })
+            console.log(comment);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    const handleInputChange = (event: any) => {
+        const { name, value } = event.target;
+        setComment({ ...comment, date: new Date(), [name]: value })
+    };
+
+    const textareaStyles: any = {
+        height: "400px",
+        resize: "none"
+    }
+
+    return (
+
+            <form onSubmit={handleSubmit} className="w-3/4 mx-auto h-fit pb-12">
+                <h1 className="font-bold text-3xl px-2 py-10">
+                   Post a Comment
+                </h1>
+
+                <div className="h-fit w-full my-5 rounded-md p-6 bg-white border border-light-border">
+                    <h1 className="text-lg py-2"> Post Comment </h1>
+                    <p className="text-xs"> Write comment here: </p>
+                    <textarea onChange={handleInputChange} style={textareaStyles} className="text-sm w-full my-2 px-2 py-1 border border-light-border rounded-md outline-none focus:ring-1 focus:ring-green-500 resize-none" name="content" placeholder="eg. You're opinion is objectively bad" required />
+                </div>
+                <button type="submit" className="bg-light-theme-green text-white rounded-lg px-4 py-1.5 hover:bg-light-theme-green-active"> Create Comment </button>
+            </form>
+    )
+}
+
+export default createComment;
