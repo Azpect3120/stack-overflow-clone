73dd1546b13687bcb49a5295329ebc46826b66eb
Azpect3120 <hhargreaves2006@gmail.com>
2023-10-24 13:18:05 -0700

    FEAT: You can now delete comments if you are the user who posted the comment

--- frontend/src/components/Comment.tsx
+++ frontend/src/components/Comment.tsx
@@ -13,9 +13,29 @@ interface Props {
 }
 
 function Comment(props: Props): JSX.Element {
+    const deleteComment = () => {
+        try {
+            fetch(`http://localhost:4000/comments/delete/${props.comment._id}`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            window.location.href = window.location.href;
+        } catch (err) {
+            console.error(err);
+        }
+
+    };
     return (
         <div className="px-24 py-4">
-            <p className="text-light-theme-green text-xs py-2"> {props.comment.author} ({props.comment.date.toLocaleDateString()}) </p>
+            <div className="flex text-xs items-center justify-between">
+                <p className="text-light-theme-green py-2"> {props.comment.author} ({props.comment.date.toLocaleDateString()}) </p>
+                { (props.comment.author === JSON.parse(localStorage.getItem("user")).username) ? 
+                    <button onClick={deleteComment} className="text-red-600 transition-all hover:bg-red-200 px-2 py-1 rounded-lg"> Delete </button>
+                    : ""
+                }
+            </div>
             <p className="text-md"> {props.comment.content} </p>
         </div>
     );
