320b93f34d2ee43124911ac31c25df16d0cec705
AvarageCodeEnjoyer <8she682GSYhaas@protonmail.com>
2023-10-29 21:14:31 -0700

    Added comment editing functionality

--- frontend/src/components/Comment.tsx
+++ frontend/src/components/Comment.tsx
@@ -1,4 +1,7 @@
+import React, { useState } from "react";
 import "../assets/css/output.css";
+
+import EditComment from "./EditComment"
 
 interface CommentObject {
     author: String;
@@ -13,6 +16,8 @@ interface Props {
 }
 
 function Comment(props: Props): JSX.Element {
+    const [isEditing, setIsEditing] = useState(false)
+
     const deleteComment = async () => {
         try {
             await fetch(
@@ -29,6 +34,7 @@ function Comment(props: Props): JSX.Element {
             console.error(err);
         }
     };
+
     return (
         <div className="px-24 py-4">
             <div className="flex text-xs items-center justify-between">
@@ -38,25 +44,45 @@ function Comment(props: Props): JSX.Element {
                     {props.comment.date.toLocaleDateString()}){" "}
                 </p>
 
-                {props.comment ? (
-                    JSON.parse(localStorage.getItem("user")).username ? (
-                        props.comment.author ==
-                        JSON.parse(localStorage.getItem("user")).username ? (
-                            <button
-                                onClick={deleteComment}
-                                className="text-red-600 transition-all hover:bg-red-200 px-2 py-1 rounded-lg"
-                            >
-                                Delete Comment
-                            </button>
-                        ) : null
-                    ) : null
+                {isEditing ? ( // Conditional rendering based on isEditing state
+                    <EditComment
+                        commentData={props.comment}
+                        postID={props.comment.postID}
+                        commentID={props.comment._id}
+                    />
                 ) : (
-                    "Loading..."
+                    <div>
+                        {props.comment ? (
+                            JSON.parse(localStorage.getItem("user")).username ? (
+                                props.comment.author ===
+                                JSON.parse(localStorage.getItem("user")).username ? (
+                                    <div>
+                                        <button
+                                            onClick={() => setIsEditing(true)}
+                                            className="text-green-600 transition-all hover:bg-green-200 px-2 py-1 rounded-lg mr-2"
+                                        >
+                                            Edit
+                                        </button>
+
+                                        <button
+                                            onClick={deleteComment}
+                                            className="text-red-600 transition-all hover:bg-red-200 px-2 py-1 rounded-lg ml-2"
+                                        >
+                                            Delete
+                                        </button>
+                                    </div>
+                                ) : null
+                            ) : null
+                        ) : (
+                            "Loading..."
+                        )}
+                    </div>
                 )}
             </div>
-            <p className="text-md"> {props.comment.content} </p>
+            {isEditing ? "" : <p className="text-md"> {props.comment.content} </p> }
         </div>
     );
 }
+
 
 export default Comment;
---
+++ frontend/src/components/EditComment.tsx
@@ -0,0 +1,76 @@
+import { SyntheticEvent, useState } from "react";
+
+interface CommentObject {
+    author: string;
+    content: string;
+    date: Date;
+}
+
+interface Props {
+    postID: string;
+    commentData: CommentObject;
+}
+
+function CreateComment(props: Props): JSX.Element {
+    const ls = localStorage.getItem("user");
+    const author: string = ls ? JSON.parse(ls).username : "";
+
+    const [comment, setComment] = useState<CommentObject>({
+        author,
+        content: props.commentData.content, // Use props.commentData.content as initial content
+        date: new Date(),
+    });
+
+    const handleSubmit = async (event: SyntheticEvent): Promise<void> => {
+        event.preventDefault();
+
+        if (localStorage.getItem("user") == null) {
+            window.location.href = "/accounts/login";
+            return;
+        }
+
+        setComment({ ...comment, date: new Date() });
+
+        try {
+            await fetch(`http://localhost:4000/comments/edit/${props.commentID}`, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(comment)
+            })
+
+            window.location.href = `/posts/${props.postID}`;
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const { name, value } = event.target;
+        setComment({ ...comment, date: new Date(), [name]: value });
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="w-3/4 mx-auto pb-6">
+        <textarea
+            onChange={handleInputChange}
+            value={comment.content}
+            className="h-24 text-sm w-full my-2 px-2 py-1 border border-light-border rounded-md outline-none focus:ring-1 focus:ring-green-500 resize-none"
+            name="content"
+            placeholder="eg. Your opinion is objectively bad"
+            required
+        />
+        <button
+            type="submit"
+            className="bg-light-theme-green text-white rounded-lg px-2 py-1.5 hover:bg-light-theme-green-active"
+        >
+            Save Comment
+        </button>
+    </form>
+    
+    
+    );
+}
+
+export default CreateComment;
