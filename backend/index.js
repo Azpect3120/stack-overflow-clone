db042b54d68af2d7c9d1c4b1409b692f094720d6
NikoDrossos <ndross427@West-MEC.org>
2023-10-30 14:20:40 -0700

    fixxed bugs

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
